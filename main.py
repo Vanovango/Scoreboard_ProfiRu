ca56ab3898a71af40c151d8282905c79c376eb2b
Vanovango <voyten2003@bk.ru>
2025-04-07 19:55:43 +0300

    modify manege window

---
+++ main.py
@@ -0,0 +1,14 @@
+from ui_files.manege_window import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+
+
+sys.exit(app.exec_())
+
